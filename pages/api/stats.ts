c106fdd5b02594588c6aca185a451e44fc17bdce
RomanOdokienko <116632924+RomanOdokienko@users.noreply.github.com>
2025-06-10 08:12:37 +0300

    Add CORS headers to stats API

--- pages/api/stats.ts
+++ pages/api/stats.ts
@@ -9,6 +9,9 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Methods", "GET")
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type")
   if (req.method !== 'GET') {
     res.status(405).end()
     return
