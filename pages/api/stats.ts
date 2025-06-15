c99e00e6af365fdcc320f983818b38dadf1d161d
RomanOdokienko <116632924+RomanOdokienko@users.noreply.github.com>
2025-06-15 11:28:07 +0300

    Update stats.ts

--- pages/api/stats.ts
+++ pages/api/stats.ts
@@ -10,8 +10,13 @@ export default async function handler(
   res: NextApiResponse
 ) {
   res.setHeader("Access-Control-Allow-Origin", "*")
-  res.setHeader("Access-Control-Allow-Methods", "GET")
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS")
   res.setHeader("Access-Control-Allow-Headers", "Content-Type")
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
 
   if (req.method !== 'GET') {
     res.status(405).end()
